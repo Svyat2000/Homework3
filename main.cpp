3e6dacb6028a73ec61cab494393c37c3d41b28b2
Svyat2000 <61850752+Svyat2000@users.noreply.github.com>
2020-03-22 21:11:54 +0300

    Задание2(e)

--- main.cpp
+++ main.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
 #include <fstream>
-#include <ctime>
+#include <cstdlib>
 using namespace std;
-int Random(int min, int max);
+
 int main(){
 
     ofstream file;
     file.open("c:\\Qt\\Projects\\1\\example4.txt");
-    srand(static_cast<unsigned int>(time(0)));
-    for (int i=0; i < 10; ++i)
-    {
-        std::cout <<Random(5,15) <<endl;
-        file<<Random(5,15) <<endl;
+    int sum=0;//>20
+    const int n=100;
+    int arr[n];
+    srand(2547);
+    for (int i=0; i<n; i++){
+        arr[i] = rand() % 56;
+        if (arr[i]>20){
+            sum+=arr[i];
 
-
-
-
-
-}
+         }
+        cout<<arr[i]<<endl;
+     }
+    cout<<"Sum i>20: "<<sum;
+    file<<sum<<endl;
     file.close();
     return 0;
 }
 
 
 
-int Random(int min, int max)
-{
-    static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
-    return static_cast<int>(rand() * fraction * (max - min + 1) + min);
-}
