0db2d3770721a5077450358b2b79cc7b07d7fd1c
Svyat2000 <61850752+Svyat2000@users.noreply.github.com>
2020-03-25 02:28:03 +0300

    Задание 12

--- main.cpp
+++ main.cpp
@@ -7,20 +7,19 @@ int main(){
 
     ofstream file;
     file.open("c:\\Qt\\Projects\\1\\example4.txt");
-    int nP=0,nO=0;
     const int n=100;
     int arr[n];
     srand(2547);
 
     for (int i=0; i<n; i++){
         arr[i] = (rand() % 101)-50;
-        if (arr[i]>0){
-           nP++;
+        if (arr[i]%2==1){
+           arr[i]+=3;
 
         }
-        if (arr[i]<0){
+        if (arr[i]%2==0){
 
-           nO++;
+           arr[i]*=2;
 
          }
 
@@ -31,8 +30,7 @@ int main(){
      }
 
 
-    cout<<"Kol-vo polog: "<<nP<<endl<<"Kol-vo otriz: "<<nO<<endl;
-    file<<"Kol-vo polog: "<<nP<<endl<<"Kol-vo otriz: "<<nO<<endl;
+
     file.close();
     return 0;
 }
