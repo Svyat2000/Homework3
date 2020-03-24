9a6efc836bf516b9b43b69e742f18c49b081e858
Svyat2000 <61850752+Svyat2000@users.noreply.github.com>
2020-03-25 02:06:21 +0300

    Задание 7

--- main.cpp
+++ main.cpp
@@ -7,20 +7,36 @@ int main(){
 
     ofstream file;
     file.open("c:\\Qt\\Projects\\1\\example4.txt");
-    int sum=0;//<4
+    double sredP=0,sredO=0,sredAll=0;
+    int nP=0,nO=0;
     const int n=100;
     int arr[n];
     srand(2547);
+
     for (int i=0; i<n; i++){
-        arr[i] = rand() % 56;
-        if (arr[i]<4){
-            sum+=arr[i];
+        arr[i] = (rand() % 101)-50;
+      if (arr[i]>0){
+           sredP+=arr[i];
+           nP++;
+
+        }
+        if (arr[i]<0){
+            sredO+=arr[i];
+           nO++;
 
          }
+        sredAll+=arr[i];
+
+
         cout<<arr[i]<<endl;
+        file<<arr[i]<<endl;
      }
-    cout<<"Sum i<4: "<<sum;
-    file<<sum<<endl;
+     sredAll=sredAll/n;
+     sredP=sredP/nP;
+     sredO=sredO/nO;
+
+    cout<<"Srednee arifm: vsex, polog, otriz = "<<endl<<sredAll<<endl<<sredP<<endl<<sredO<<endl;
+    file<<"Srednee arifm: vsex, polog, otriz = "<<endl<<sredAll<<endl<<sredP<<endl<<sredO<<endl;
     file.close();
     return 0;
 }
