4c80a7db7fdc7501f5ad32cb508c85fde777b59b
Svyat2000 <61850752+Svyat2000@users.noreply.github.com>
2020-03-25 02:13:55 +0300

    Задание8

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ int main(){
 
     ofstream file;
     file.open("c:\\Qt\\Projects\\1\\example4.txt");
-    double sredP=0,sredO=0,sredAll=0;
     int nP=0,nO=0;
     const int n=100;
     int arr[n];
@@ -15,28 +14,25 @@ int main(){
 
     for (int i=0; i<n; i++){
         arr[i] = (rand() % 101)-50;
-      if (arr[i]>0){
-           sredP+=arr[i];
+        if (arr[i]>0){
            nP++;
 
         }
         if (arr[i]<0){
-            sredO+=arr[i];
+
            nO++;
 
          }
-        sredAll+=arr[i];
+
 
 
         cout<<arr[i]<<endl;
         file<<arr[i]<<endl;
      }
-     sredAll=sredAll/n;
-     sredP=sredP/nP;
-     sredO=sredO/nO;
 
-    cout<<"Srednee arifm: vsex, polog, otriz = "<<endl<<sredAll<<endl<<sredP<<endl<<sredO<<endl;
-    file<<"Srednee arifm: vsex, polog, otriz = "<<endl<<sredAll<<endl<<sredP<<endl<<sredO<<endl;
+
+    cout<<"Kol-vo polog: "<<nP<<endl<<"Kol-vo otriz: "<<nO<<endl;
+    file<<"Kol-vo polog: "<<nP<<endl<<"Kol-vo otriz: "<<nO<<endl;
     file.close();
     return 0;
 }
