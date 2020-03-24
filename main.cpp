5b8633a843fe846774f1397dfe0b7a54462783a1
Svyat2000 <61850752+Svyat2000@users.noreply.github.com>
2020-03-25 02:56:51 +0300

    Задание6

--- main.cpp
+++ main.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <math.h>
 using namespace std;
 
 int main(){
 
     ofstream file;
     file.open("c:\\Qt\\Projects\\1\\example4.txt");
-    const int n=100;
+    const int n=20;
     int arr[n];
-    srand(2547);
+    srand(1365);
+    int nmax = 0, nmin=0;
 
     for (int i=0; i<n; i++){
         arr[i] = (rand() % 101)-50;
-        if (arr[i]%2==1){
-           arr[i]+=3;
-
-        }
-        if (arr[i]%2==0){
-
-           arr[i]*=2;
-
-         }
-
 
 
         cout<<arr[i]<<endl;
         file<<arr[i]<<endl;
-     }
-
-
+        }
+     for (int i=1;i<n;i++) {
+         if(fabs(nmax)<fabs(arr[i])){
+            nmax=arr[i];
+          }
+         if(fabs(nmin)>fabs(arr[i])){
+            nmin=arr[i];
+        }
+    }
+    cout<<"Max module number: "<<nmax<<endl<<"Min module number: "<<nmin<<endl;
+    file<<"Max module number: "<<nmax<<endl<<"Min module number: "<<nmin<<endl;
 
     file.close();
     return 0;
